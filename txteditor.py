37372b90b985d786e80e5b87b1a1115416dec493
eukap <esk.geek@gmail.com>
2019-04-06 06:41:32 +0300

    Perform refactoring
    Creates the TextFrameTab class. From this moment every text tab is an
    instance of this class. It allows to preserve states of menu items
    and other widgets in separate objects.

--- txteditor.py
+++ txteditor.py
@@ -5,6 +5,38 @@ from pathlib import Path
 
 
 class Application(Frame):
+    class TextFrameTab:
+        def __init__(self, obj):
+            self.text_frm = Frame(obj.notebook)
+            self.text_frm.pack(side=TOP, expand=YES, fill=BOTH)
+
+            self.text = Text(self.text_frm)
+            self.text.config(fg='#111111', bg='#eeeeee', bd=0, wrap=WORD)
+            self.text.focus()
+            self.yscroll = Scrollbar(self.text_frm, orient=VERTICAL)
+            self.yscroll.config(cursor='arrow', command=self.text.yview,
+                                bg='#5d5d5d', activebackground='#6e6e6e')
+            self.text['yscrollcommand'] = self.yscroll.set
+            self.yscroll.pack(side=RIGHT, fill=Y)
+            self.text.pack(side=LEFT, expand=YES, fill=BOTH)
+            obj.notebook.add(self.text_frm, padding=1, text=obj.filenames[obj.tabcount])
+            obj.notebook.select(obj.tabcount)
+
+            self.text.edit_modified(arg=False)
+            self.file_menu = obj.file_menu
+            self.file_menu.entryconfigure(4, state=DISABLED)
+            self.text.bind('<FocusIn>', self.check_state)
+            self.text.bind('<FocusOut>', self.check_state)
+
+        def check_state(self, event):
+            event.file_menu = self.file_menu
+            modified = self.text.edit_modified()
+            if modified:
+                event.file_menu.entryconfigure(4, state=ACTIVE)
+                event.file_menu.entryconfigure(0, state=ACTIVE)
+            else:
+                event.file_menu.entryconfigure(4, state=DISABLED)
+
     def __init__(self, parent=None):
         Frame.__init__(self, parent)
         # A list with names of files opened in separate tabs
@@ -24,75 +56,59 @@ class Application(Frame):
         self.style.configure('TNotebook', background='#606060')
 
         self.notebook = Notebook(self)
+        self.notebook.enable_traversal()
         self.notebook.pack(side=TOP, expand=YES, fill=BOTH)
 
-        self.text_frm = Frame(self.notebook)
-        self.text_frm.pack(side=TOP, expand=YES, fill=BOTH)
+        self.file_menubtn = Menubutton(self.menubar)
+        self.file_menubtn.config(text='File', activebackground='#647899',
+                                 bg='#444444', underline=0)
+        self.file_menubtn.pack(side=LEFT)
+        self.file_menu = Menu(self.file_menubtn, tearoff=0)
+        self.file_menubtn['menu'] = self.file_menu
+        self.file_menu.config(activebackground='#647899', bg='#444444')
+        self.file_menu.add_command(label='New', command=self.create_newdoc)
+        self.file_menu.add_command(label='Open', command=self.open_file)
+        self.file_menu.add_command(label='Close')
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label='Save', command=self.save_file)
+        self.file_menu.add_command(label='Save As',
+                                   command=self.save_as_file)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label='Exit', command=exit)
 
-        self.text = Text(self.text_frm)
-        self.text.config(fg='#111111', bg='#eeeeee', bd=0, wrap=WORD)
-        self.text.focus()
-        self.yscroll = Scrollbar(self.text_frm, orient=VERTICAL)
-        self.yscroll.config(cursor='arrow', command=self.text.yview,
-                            bg='#5d5d5d', activebackground='#6e6e6e')
-        self.text['yscrollcommand'] = self.yscroll.set
-        self.yscroll.pack(side=RIGHT, fill=Y)
-        self.text.pack(side=LEFT, expand=YES, fill=BOTH)
+        self.edit_menubtn = Menubutton(self.menubar)
+        self.edit_menubtn.config(text='Edit', activebackground='#647899',
+                                 bg='#444444', underline=0)
+        self.edit_menubtn.pack(side=LEFT)
+        self.edit_menu = Menu(self.edit_menubtn, tearoff=0)
+        self.edit_menubtn['menu'] = self.edit_menu
+        self.edit_menu.config(activebackground='#647899', bg='#444444')
+        self.edit_menu.add_command(label='Undo')
+        self.edit_menu.add_command(label='Redo')
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label='Cut', command=self.cut_text)
+        self.edit_menu.add_command(label='Copy', command=self.copy_text)
+        self.edit_menu.add_command(label='Paste', command=self.paste_text)
+        self.edit_menu.add_command(label='Delete', command=self.del_text)
+        self.edit_menu.add_separator()
+        self.edit_menu.add_command(label="Select All",
+                                   command=self.select_all)
 
-        self.notebook.add(self.text_frm, padding=1,
-                          text=self.filenames[self.tabcount])
+        self.view_menubtn = Menubutton(self.menubar)
+        self.view_menubtn.config(text='View', activebackground='#647899',
+                                 bg='#444444', underline=0)
+        self.view_menubtn.pack(side=LEFT)
+        self.view_menu = Menu(self.view_menubtn, tearoff=0)
+        self.view_menubtn['menu'] = self.view_menu
+        self.view_menu.config(activebackground='#647899', bg='#444444')
 
-        self.filemenu = Menubutton(self.menubar)
-        self.filemenu.config(text='File', activebackground='#647899',
-                             bg='#444444', underline=0)
-        self.filemenu.pack(side=LEFT)
-        self.filemenu.menu = Menu(self.filemenu, tearoff=0)
-        self.filemenu['menu'] = self.filemenu.menu
-        self.filemenu.menu.config(activebackground='#647899', bg='#444444')
-        self.filemenu.menu.add_command(label='New')
-        self.filemenu.menu.add_command(label='Open', command=self.open_file)
-        self.filemenu.menu.add_command(label='Close')
-        self.filemenu.menu.add_separator()
-        self.filemenu.menu.add_command(label='Save', command=self.save_file,
-                                       state=DISABLED)
-        self.filemenu.menu.add_command(label='Save As',
-                                       command=self.save_as_file)
-        self.filemenu.menu.add_separator()
-        self.filemenu.menu.add_command(label='Exit', command=exit)
-
-        self.editmenu = Menubutton(self.menubar)
-        self.editmenu.config(text='Edit', activebackground='#647899',
-                             bg='#444444', underline=0)
-        self.editmenu.pack(side=LEFT)
-        self.editmenu.menu = Menu(self.editmenu, tearoff=0)
-        self.editmenu['menu'] = self.editmenu.menu
-        self.editmenu.menu.config(activebackground='#647899', bg='#444444')
-        self.editmenu.menu.add_command(label='Undo')
-        self.editmenu.menu.add_command(label='Redo')
-        self.editmenu.menu.add_separator()
-        self.editmenu.menu.add_command(label='Cut', command=self.cut_text)
-        self.editmenu.menu.add_command(label='Copy', command=self.copy_text)
-        self.editmenu.menu.add_command(label='Paste', command=self.paste_text)
-        self.editmenu.menu.add_command(label='Delete', command=self.del_text)
-        self.editmenu.menu.add_separator()
-        self.editmenu.menu.add_command(label="Select All",
-                                       command=self.select_all)
-
-        self.viewmenu = Menubutton(self.menubar)
-        self.viewmenu.config(text='View', activebackground='#647899',
-                             bg='#444444', underline=0)
-        self.viewmenu.pack(side=LEFT)
-        self.viewmenu.menu = Menu(self.viewmenu, tearoff=0)
-        self.viewmenu['menu'] = self.viewmenu.menu
-        self.viewmenu.menu.config(activebackground='#647899', bg='#444444')
-
-        self.helpmenu = Menubutton(self.menubar)
-        self.helpmenu.config(text='Help', activebackground='#647899',
-                             bg='#444444', underline=0)
-        self.helpmenu.pack(side=LEFT)
-        self.helpmenu.menu = Menu(self.helpmenu, tearoff=0)
-        self.helpmenu['menu'] = self.helpmenu.menu
-        self.helpmenu.menu.config(activebackground='#647899', bg='#444444')
+        self.help_menubtn = Menubutton(self.menubar)
+        self.help_menubtn.config(text='Help', activebackground='#647899',
+                                 bg='#444444', underline=0)
+        self.help_menubtn.pack(side=LEFT)
+        self.help_menu = Menu(self.help_menubtn, tearoff=0)
+        self.help_menubtn['menu'] = self.help_menu
+        self.help_menu.config(activebackground='#647899', bg='#444444')
 
         self.quit_btn = Button(self.toolbar)
         self.quit_btn.config(text='Quit', font=('Sans', '10'), fg='#eeeeee',
@@ -101,41 +117,33 @@ class Application(Frame):
                              pady=2)
         self.quit_btn.pack(side=RIGHT)
 
+        self.tab = self.TextFrameTab(self)
+
+    def create_newdoc(self):
+        self.tabcount += 1
+        self.filenames.append('Untitled')
+        self.tab = self.TextFrameTab(self)
+
     def open_file(self):
         filepath = askopenfilename()
         if filepath:
-            modified = self.text.edit_modified()
+            modified = self.tab.text.edit_modified()
             p = Path(filepath)
             filename = p.parts[-1]
 
             if self.filenames[self.tabcount] == 'Untitled' and not modified:
-                self.notebook.hide(self.tabcount)
+                self.notebook.forget(self.tabcount)
+                del self.filenames[self.tabcount]
+                self.tabcount -= 1
 
-            self.tabcount += 1
             self.filenames.append(filename)
-
-            self.text_frm = Frame(self.notebook)
-            self.text_frm.pack(side=TOP, expand=YES, fill=BOTH)
-
-            self.text = Text(self.text_frm)
-            self.text.config(fg='#111111', bg='#eeeeee', bd=0, wrap=WORD)
-            self.text.focus()
-            self.yscroll = Scrollbar(self.text_frm, orient=VERTICAL)
-            self.yscroll.config(cursor='arrow', command=self.text.yview,
-                                bg='#5d5d5d', activebackground='#6e6e6e')
-            self.text['yscrollcommand'] = self.yscroll.set
-            self.yscroll.pack(side=RIGHT, fill=Y)
-            self.text.pack(side=LEFT, expand=YES, fill=BOTH)
-
-            self.notebook.add(self.text_frm, padding=1,
-                              text=self.filenames[self.tabcount])
-            self.notebook.select(self.tabcount)
+            self.tabcount += 1
+            self.tab = self.TextFrameTab(self)
 
             file = open(filepath)
-            self.text.insert(1.0, file.read())
+            self.tab.text.insert(1.0, file.read())
             file.close()
-
-            self.text.edit_modified(arg=False)
+            self.tab.text.edit_modified(arg=False)
 
     def save_file(self):
         pass
@@ -143,43 +151,46 @@ class Application(Frame):
     def save_as_file(self):
         filepath = asksaveasfilename()
         if filepath:
-            text = self.text.get(1.0, END)
+            tab = self.focus_lastfor()
+            text = tab.get(1.0, END)
             file = open(filepath, 'w')
             file.write(text)
             file.close()
+            self.file_menu.entryconfigure(4, state=DISABLED)
+            tab.edit_modified(arg=False)
 
     def cut_text(self):
         try:
-            text = self.text.get(SEL_FIRST, SEL_LAST)
-            self.text.delete(SEL_FIRST, SEL_LAST)
-            self.text.clipboard_clear()
-            self.text.clipboard_append(text)
+            text = self.tab.text.get(SEL_FIRST, SEL_LAST)
+            self.tab.text.delete(SEL_FIRST, SEL_LAST)
+            self.tab.text.clipboard_clear()
+            self.tab.text.clipboard_append(text)
         except TclError:
             pass
 
     def copy_text(self):
         try:
-            text = self.text.get(SEL_FIRST, SEL_LAST)
-            self.text.clipboard_clear()
-            self.text.clipboard_append(text)
+            text = self.tab.text.get(SEL_FIRST, SEL_LAST)
+            self.tab.text.clipboard_clear()
+            self.tab.text.clipboard_append(text)
         except TclError:
             pass
 
     def paste_text(self):
         try:
-            text = self.text.selection_get(selection='CLIPBOARD')
-            self.text.insert(INSERT, text)
+            text = self.tab.text.selection_get(selection='CLIPBOARD')
+            self.tab.text.insert(INSERT, text)
         except TclError:
             pass
 
     def del_text(self):
         try:
-            self.text.delete(SEL_FIRST, SEL_LAST)
+            self.tab.text.delete(SEL_FIRST, SEL_LAST)
         except TclError:
             pass
 
     def select_all(self):
-        self.text.tag_add(SEL, 1.0, END)
+        self.tab.text.tag_add(SEL, 1.0, END)
 
 
 def main():
